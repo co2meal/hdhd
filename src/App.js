5176011e6f3e162decc7483ef2b4208d3cacb24c
nicola <nicola@zoyi.co>
2018-03-07 18:16:27 +0900

    Remove unused resources

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Button } from 'semantic-ui-react';
 
@@ -7,10 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Reaicxx</h1>
-        </header>
         <p className="App-intro">
           <Button primary={true}>
             To get started, edit <code>src/App.jss</code> and se to relodi.
